8a8be3a21259a0edc8b6fe490f7201812215820d
jcorozco73 <jc.martin.orozco@gmail.com>
2023-04-27 12:00:05 -0300

    ultima entrega proyecto final

--- js/main.js
+++ js/main.js
@@ -186,6 +186,14 @@ const productos = [
 
 
 
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+
+
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
